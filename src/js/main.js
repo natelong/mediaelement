54e7790cd66c5a2b8bac2ff35f5099c6b0318257
Nate Long <long.nathaniel@gmail.com>
2012-04-12 17:49:14 -0700

    added initializer to main script

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,13 @@
 #include "me-featuredetection.js"
 #include "me-mediaelements-lite.js"
 #include "me-shim.js"
+
+$('video').each(function(){
+	new mejs.MediaElement( this, {
+		// remove or reorder to change plugin priority
+		plugins: ['flash'],
+		pluginVars: [ 'autohide=false','preload=auto' ],
+		// name of flash file
+		flashName: 'flashmediaelement.swf'
+	});
+});
